f3427eab4d868f2688f024bedb5914088552bd40
Liam Owen <l1amow3n5@yahoo.com>
2019-02-04 12:14:14 -0700

    made a pikachu class

---
+++ src/pokemon/model/Pikachu.java
@@ -0,0 +1,19 @@
+package pokemon.model;
+
+public class Pikachu extends Pokemon implements Electric
+{
+	public Pikachu (int number, String name)
+	{
+		super(number, name);
+	}
+	
+	public void stun()
+	{
+		System.out.println("He is unable to move");
+	}
+	
+	public int shock()
+	{
+		return 123445; 
+	}
+}
