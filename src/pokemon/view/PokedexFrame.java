cf0e3d90741b922bfa4520aa4e557fce5e6fe3b7
Liam Owen <l1amow3n5@yahoo.com>
2019-02-04 13:54:46 -0700

    finished the frame code

--- src/pokemon/view/PokedexFrame.java
+++ src/pokemon/view/PokedexFrame.java
@@ -5,5 +5,25 @@ import pokemon.controller.PokedexController;
 
 public class PokedexFrame
 {
-
+	private PokedexController app;
+	private PokedexPanel panel;
+	
+	public PokedexFrame (PokedexController app)
+	{
+		super();
+		
+		this.app = app;
+		this.panel = new PokedexPanel(app);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(panel);
+		this.setTitle("Pokemon Frame");
+		this.setSize(800, 600);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
--- src/pokemon/view/PokedexPanel.java
+++ src/pokemon/view/PokedexPanel.java
@@ -1,6 +1,65 @@
 package pokemon.view;
 
-public class PokedexPanel
-{
+import javax.swing.*;
+import pokemon.controller.PokedexController;
+import java.awt.event.*;
+import java.awt.*;
 
+public class PokedexPanel extends JPanel
+{
+	private PokedexController app; 
+	private SpringLayout appLayout;
+	
+	private JTextField PikachuText;
+	private JTextField CharizardText; 
+	private JTextField BulbasaurText; 
+	private JTextField SquirtleText; 
+	private JTextField JigglypuffText;
+	
+	private JLabel PikachuLabel;
+	private JLabel CharizardLabel;
+	private JLabel BulbasaurLabel;
+	private JLabel SquirtleLabel;
+	private JLabel JigglypuffLabel;
+	
+	public PokedexPanel(PokedexController app)
+	{
+		super();
+		
+		this.app = app;
+		PikachuText = new JTextField("Pik");
+		CharizardText = new JTextField("Char");
+		BulbasaurText = new JTextField("Bulb");
+		SquirtleText = new JTextField("Squi");
+		JigglypuffText = new JTextField("Jiggy");
+		
+		PikachuLabel = new JLabel("Pikachu");
+		CharizardLabel = new JLabel("Charizard");
+		BulbasaurLabel = new JLabel("Bulbasaur");
+		SquirtleLabel = new JLabel("Squirtle");
+		JigglypuffLabel = new JLabel("Jigglypuff");
+		
+		
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(appLayout);
+		this.setPreferredSize(new Dimension(800, 600));
+		this.setBackground(Color.RED);
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
